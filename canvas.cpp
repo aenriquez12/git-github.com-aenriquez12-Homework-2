c5dc872860e8b52b99fd7b88c771c733c6c0e834
aenriquez12 <tenriquez1818@gmail.com>
2024-09-21 22:05:12 -0500

    changed canvas

--- canvas.cpp
+++ canvas.cpp
@@ -36,117 +36,67 @@ Canvas::Canvas(int width)
 
 
 
-Canvas::Canvas(char x)
-{
-  
-  _width = 5;                                              // Assign width values
-  
-  
-  
- 
-  C = new char*[5];                                         //created dynamic character 2D array
-  for(int i= 0; i< 5; ++i)
-  {
-    C[i] = new char [_width];    
-    for(int j = 0; j<_width; j++) C[i][j] = ' ';            // initialize current row's cells with white space characters
-  }
-
-   
-  switch(x)                                                 // creates different cases depending on the letter inputed an
-  {
-    case 'A':                                               // If the input is 'A'
-      {
-
-      
-        C[0][1] = '#';
-        C[0][2] = '#';
-        C[0][3] = '#';
-
-        C[1][0] = '#';
-        C[1][4] = '#';
-
-        C[2][0] = '#';
-        C[2][1] = '#';
-        C[2][2] = '#';
-        C[2][3] = '#';
-        C[2][4] = '#';
-
-        C[3][0] = '#';
-        C[3][4] = '#';
-
-        C[4][0] = '#';
-        C[4][4] = '#';
-      }
-        break;
-    case 'B':                                           // If the input is 'B'
-      {      
-        C[0][0] = '#';
-        C[0][1] = '#';
-        C[0][2] = '#';
-        C[0][3] = '#';
-
-        C[1][0] = '#';
-        C[1][4] = '#';
-       
-        C[2][0] = '#';
-        C[2][1] = '#';
-        C[2][2] = '#';
-        C[2][3] = '#';
-
-        C[3][0] = '#';
-        C[3][4] = '#';
-
-        C[4][0] = '#';
-        C[4][1] = '#';
-        C[4][2] = '#';
-        C[4][3] = '#';
-      }
-        break;
-
-    case 'C':                                               // If the input is 'C'
-      {
-        C[0][1] = '#';
-        C[0][2] = '#';
-        C[0][3] = '#';
-        C[0][4] = '#';
-
-        C[1][0] = '#';
-
-        C[2][0] = '#';
-
-        C[3][0] = '#';
-
-        C[4][1] = '#';
-        C[4][2] = '#';
-        C[4][3] = '#';
-        C[4][4] = '#';
-      }
-        break;
-    case 'D':                                       // If the input is 'D'
-    {
-        C[0][0] = '#';
-        C[0][1] = '#';
-        C[0][2] = '#';
-        C[0][3] = '#';
-
-        C[1][0] = '#';
-        C[1][4] = '#';
-       
-        C[2][0] = '#';
-        C[2][4] = '#';
-
-        C[3][0] = '#';
-        C[3][4] = '#';
-
-        C[4][0] = '#';
-        C[4][1] = '#';
-        C[4][2] = '#';
-        C[4][3] = '#';
+Canvas::Canvas(char x) {
+    _width = 5; // width is fixed at 5
+    C = new char*[5];
+    for (int i = 0; i < 5; ++i) {
+        C[i] = new char[_width];
     }
-        break;
 
-  }
+    // Initialize all elements to ' ' (space)
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < _width; ++j) {
+            C[i][j] = ' ';
+        }
+    }
 
+    // Define patterns based on the character passed ('A', 'B', 'C', 'D')
+    if (x == 'A') {
+        // Pattern for 'A'
+        for (int i = 1; i < 4; ++i) {
+            C[0][i] = '#';
+        }
+        C[1][0] = C[1][4] = '#';
+        for (int i = 0; i < 5; ++i) {
+            C[2][i] = '#';
+        }
+        C[3][0] = C[3][4] = '#';
+        C[4][0] = C[4][4] = '#';
+    } 
+    else if (x == 'B') {
+        // Pattern for 'B'
+        for (int i = 0; i < 4; ++i) {
+            C[0][i] = '#';
+            C[2][i] = '#';
+            C[4][i] = '#';
+        }
+        C[1][0] = C[1][4] = '#';
+        C[3][0] = C[3][4] = '#';
+    }
+    else if (x == 'C') {
+        // Pattern for 'C'
+        for (int i = 1; i < 5; ++i) {
+            C[0][i] = '#';
+            C[4][i] = '#';
+        }
+        C[1][0] = C[3][0] = C[2][0] = '#';
+    }
+    else if (x == 'D') {
+        // Pattern for 'D'
+        for (int i = 0; i < 4; ++i) {
+            C[0][i] = C[4][i] = '#';
+        }
+        C[1][0] = C[1][4] = '#';
+        C[2][0] = C[2][4] = '#';
+        C[3][0] = C[3][4] = '#';
+    } else {
+        // Default to a blank canvas if another character is provided
+        for (int i = 0; i < 5; ++i) {
+            for (int j = 0; j < 5; ++j) {
+                C[i][j] = ' ';
+            }
+        }
+    }
 }
 
 
